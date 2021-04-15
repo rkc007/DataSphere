c1666fc06e83ca22a2fe7d7d47c61c57bd9669e8
Rahul Chauhan <rahulchauhan819@gmail.com>
2021-04-14 22:00:50 -0500

    backend: Added ES6 + nodemon

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,9 @@
-var express = require("express");
-var router = express.Router();
+import express from "express";
 
-router.get("/", function(req, res, next) {
-  return res.status(200).json({ message: "Welcome to Express API template" });
-});
-module.exports = router;
+const indexRouter = express.Router();
+
+indexRouter.get("/", (req, res) =>
+  res.status(200).json({ message: "Welcome to Express API template" })
+);
+
+export default indexRouter;
