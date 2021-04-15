7e037f138c9e2025d2d242f05d4cf42a4d277d87
Rahul Chauhan <rahulchauhan819@gmail.com>
2021-04-14 22:30:27 -0500

    backend: configured nodemon+eslint+preetier

--- backend/src/bin/www.js
+++ backend/src/bin/www.js
@@ -8,7 +8,7 @@ import app from "../app";
 /**
  * Normalize a port into a number, string, or false.
  */
-const normalizePort = val => {
+const normalizePort = (val) => {
   const port = parseInt(val, 10);
   if (Number.isNaN(port)) {
     // named pipe
@@ -35,7 +35,7 @@ const server = http.createServer(app);
 /**
  * Event listener for HTTP server "error" event.
  */
-const onError = error => {
+const onError = (error) => {
   if (error.syscall !== "listen") {
     throw error;
   }
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,10 @@
-import express from "express";
+import express from 'express';
+import { testEnvironmentVariable } from '../settings';
 
 const indexRouter = express.Router();
 
-indexRouter.get("/", (req, res) =>
-  res.status(200).json({ message: "Welcome to Express API template" })
+indexRouter.get('/', (req, res) =>
+  res.status(200).json({ message: testEnvironmentVariable })
 );
 
 export default indexRouter;
